2c79348cd6d04ec7b00b3f7e6118fd83f2cc36ba
phucdoitoan <phucdoitoan@gmail.com>
2019-12-05 22:05:20 +0900

    CHECKPOINT: about to investigate main.py; Fixed Evaluate according to cogdl

--- test_evaluate_new.py
+++ test_evaluate_new.py
@@ -15,7 +15,7 @@ def numerical_integral(l_x, l_y):
 
     return integral
 
-def main(order=1, AUC_file='data/auc/AUC_pytorch_fb_order=1.png', embed_file= 'data/embedding=pytorch_fb_remained_order-1.pkl', G_full_file='data/facebook_combined.pkl', G_remained_file='data/facebook_remained.pkl', G_removed_file='data/facebook_removed.pkl'):
+def main(order=1, AUC_file='data/auc/AUC_tensorflow_fb_order=1.png', embed_file= 'data/embedding=tensorflow_fb_remained_order-1.pkl', G_full_file='data/facebook_combined.pkl', G_remained_file='data/facebook_remained.pkl', G_removed_file='data/facebook_removed.pkl'):
     G_full = nx.read_gpickle(G_full_file)
     #G_remained = nx.read_gpickle(G_remained_file)
     G_removed = nx.read_gpickle(G_removed_file)
@@ -27,6 +27,7 @@ def main(order=1, AUC_file='data/auc/AUC_pytorch_fb_order=1.png', embed_file= 'd
 
     print('Loaded embeds')
 
+    t1_neg = timeit.default_timer()
     negative_set = set()
     while len(negative_set) < len(G_removed.edges):
         while True:
@@ -36,7 +37,8 @@ def main(order=1, AUC_file='data/auc/AUC_pytorch_fb_order=1.png', embed_file= 'd
                 negative_set.add((x, y))
                 break
 
-    print('Done randomly choosing negative edges')
+    t2_neg = timeit.default_timer()
+    print('Done randomly choosing negative edges: %.2f s' %(t2_neg - t1_neg))
 
     false_edges = list(negative_set)
     true_edges = list(G_removed.edges)
@@ -54,7 +56,7 @@ def main(order=1, AUC_file='data/auc/AUC_pytorch_fb_order=1.png', embed_file= 'd
 
     fig = plt.figure()
     plt.plot([0.0, 1.0], [0.0, 1.0], 'k--')
-    label = 'fb-LINE-tensorflow AUC %s: %.4f' % (str(order), auc_score)
+    label = 'facebook order-%s AUC: %.4f' % (str(order), auc_score)
     plt.plot(fpr, tpr, 'b-', label=label)
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
@@ -64,4 +66,12 @@ def main(order=1, AUC_file='data/auc/AUC_pytorch_fb_order=1.png', embed_file= 'd
 
     return auc_score, f1_score, auc
 
-main()
+
+order=1
+AUC_file= 'data/auc/AUC_tensorflow-2-1_fb_order=1.png'
+embed_file= 'data/embedding=tensorflow-2_fb_remained_order-1.pkl'
+G_full_file='data/facebook_combined.pkl'
+G_remained_file='data/facebook_remained.pkl'
+G_removed_file='data/facebook_removed.pkl'
+
+main(order, AUC_file, embed_file, G_full_file, G_remained_file, G_removed_file)
